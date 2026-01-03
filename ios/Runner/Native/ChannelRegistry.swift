36960a01413fab43bb1eab5012c77297e4b13d5e
hhhhhashi <r.hashiwbc8091025@gmail.com>
2026-01-04 00:58:30 +0900

    修正

--- ios/Runner/Native/ChannelRegistry.swift
+++
@@ -1,9 +0,0 @@
-import Flutter
-
-final class ChannelRegistry {
-
-  static func register(with controller: FlutterViewController) {
-    ShareService.register(with: controller)
-    NotificationPermissionService.register(with: controller)
-  }
-}
